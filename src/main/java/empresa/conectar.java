ec6fb373cad6e334106b9f96ff994a144f7c9027
antonio.eslava <anesher2002@gmail.com>
2024-05-29 17:22:35 +0200

    segunda subida

---
+++ src/main/java/empresa/conectar.java
@@ -0,0 +1,25 @@
+package empresa;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class conectar {
+    private static final String CONTROLADOR = "com.mysql.jdbc.Driver";
+    private static final String URL = "jdbc:mysql://localhost:4232/empresa?allowPublicKeyRetrieval=true&useSSL=false";
+    private static final String USUARIO = "root";
+    private static final String CLAVE = "1234";
+
+    public static Connection conectar() {
+        Connection conexion = null;
+        try {
+            conexion = (Connection) DriverManager.getConnection(URL, USUARIO, CLAVE);
+            System.out.println("Conexión OK");
+        } catch (SQLException e) {
+            System.out.println("Error en la conexión");
+            e.printStackTrace();
+        }
+        return conexion;
+    }
+
+}
