a24d5e4dc06ef9962037f8f3ccc74aee5193451b
antonio.eslava <anesher2002@gmail.com>
2024-05-30 20:12:52 +0200

    subida

--- src/main/java/empresa/Metodos.java
+++ src/main/java/empresa/Metodos.java
@@ -3,6 +3,19 @@ package empresa;
 import java.sql.*;
 
 public class Metodos implements Interfaz {
+
+    public void borrarTabla() {
+        String sqlDrop = "DROP TABLE IF EXISTS usuarios";
+        try (Connection cn = Conectar.conectar();
+             Statement stm = cn.createStatement()) {
+
+            stm.executeUpdate(sqlDrop);
+            System.out.println("Tabla usuarios borrada correctamente");
+        } catch (SQLException e) {
+            System.out.println("No se pudo borrar la tabla usuarios");
+            e.printStackTrace();
+        }
+    }
     public void crearTabla() {
         Connection cn = null;
         Statement stm = null;
@@ -11,16 +24,15 @@ public class Metodos implements Interfaz {
         try {
             cn = Conectar.conectar();
             stm = cn.createStatement();
-            resultado = stm.executeQuery("SHOW TABLES LIKE 'dietas';");
+            resultado = stm.executeQuery("SHOW TABLES LIKE 'usuarios';");
             //para ver si esta la tabla hecha
             if (!resultado.next()) {
-                String sql = "CREATE TABLE dietas ("
-                        + "id INT NOT NULL AUTO_INCREMENT,"
-                        + "empleado VARCHAR(100) NOT NULL, "
-                        + "departamento VARCHAR(100) NOT NULL,"
-                        + "importe FLOAT NOT NULL,"
-                        + "concepto VARCHAR(255) NOT NULL,"
-                        + "PRIMARY KEY (id));";
+                String sql = "CREATE TABLE usuarios ("
+                        + "id_user INT NOT NULL AUTO_INCREMENT,"
+                        + "username VARCHAR(200) NOT NULL, "
+                        + "password INT NOT NULL,"
+                        + "permisos VARCHAR(255) NOT NULL,"
+                        + "PRIMARY KEY (id_user));";
                 stm.executeUpdate(sql);
                 System.out.println("Tabla creada correctamente");
             } else {
@@ -40,7 +52,7 @@ public class Metodos implements Interfaz {
         try {
             cn = Conectar.conectar();
             stm = cn.createStatement();
-            stm.executeUpdate("INSERT INTO dietas (empleado, departamento, importe, concepto) VALUES ('jose', 'comercial', 300, 'Envio rapido');");
+            stm.executeUpdate("INSERT INTO usuarios (username, password, permisos) VALUES ('antonio', 6525, 'todos');");
             System.out.println("Usuario insertado correctamente");
         } catch (SQLException e) {
             System.out.println("Usuario no se ha podido agregar");
--- src/main/java/empresa/main.java
+++ src/main/java/empresa/main.java
@@ -1,16 +1,12 @@
 package empresa;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
 
 public class main {
     public static void main(String[] args) {
       Conectar.conectar();
 
         Metodos metodos= new Metodos();
-        metodos.crearTabla();
-        metodos.insertarDato();
+       metodos.crearTabla();
+       metodos.insertarDato();
     }
 }
